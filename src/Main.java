f6f54046ce7e5099abe1deee5d90aae1aac3fd84
XiujunYang <proverb70@gmail.com>
2017-01-19 23:35:22 +0400

    remove unused variable

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
-    static int countSquares = 0, countRectangles = 0, countPolygons=0;
+
     public static void main(String[] args){
         String[] str = {"pear",
                 "amleth",
